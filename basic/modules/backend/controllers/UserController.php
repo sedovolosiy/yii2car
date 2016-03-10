d5d60f8e640cdf9ed9ff6624a3ee8f4f289dbb4e
Siedovolosij Serhij <sedovolosiy@gmail.com>
2016-03-10 17:55:25 +0200

    добавил условие скрывающие основной контент при авторизации и редирект на страницу yii.loc/admin

--- basic/modules/backend/controllers/UserController.php
+++ basic/modules/backend/controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends CustomController
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+            return $this->render('cp');
         }
         return $this->render('login', [
             'model' => $model,
